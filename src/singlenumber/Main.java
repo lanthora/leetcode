0682c004b5bc2c1fa962aedc3af45f4395f7306f
lanthora <nierunjie@gmail.com>
2019-03-14 14:09:38 +0800

    Single Number

---
+++ src/singlenumber/Main.java
@@ -0,0 +1,9 @@
+package singlenumber;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] numbers = new int[]{4, 1, 2, 1, 2};
+        Solution solution = new Solution();
+        System.out.println(solution.singleNumber(numbers));
+    }
+}
